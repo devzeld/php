d2b78b7239e92aacd04051b1b99c76cc27b972cf
devzeld <zeld.exe.dev@gmail.com>
2025-12-16 09:50:12 +0100

    Add email field to users table and update registration form; modify SQL queries for user creation

--- pronto/assets/php/connect.php
+++ pronto/assets/php/connect.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $conn = $pdo->query("CREATE TABLE IF NOT EXISTS users(id int NOT NULL PRIMARY KEY AUTO_INCREMENT , username VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), surname VARCHAR(255), password VARCHAR(255), create_time DATETIME);");
+    $conn = $pdo->query("CREATE TABLE IF NOT EXISTS users(mail VARCHAR(255) UNIQUE, id int NOT NULL PRIMARY KEY AUTO_INCREMENT , username VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), surname VARCHAR(255), password VARCHAR(255), create_time DATETIME);");
     $conn = $pdo->query("CREATE TABLE IF NOT EXISTS products(id int NOT NULL PRIMARY KEY AUTO_INCREMENT, product_name VARCHAR(255), stock INT, price INT, create_time DATETIME);");
     $conn = $pdo->query("CREATE TABLE IF NOT EXISTS puchases(id int NOT NULL PRIMARY KEY AUTO_INCREMENT, user_id INT NOT NULL, product_id INT NOT NULL, quantity INT, create_time DATETIME, FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (product_id) REFERENCES products(id));");
 
--- pronto/user.php
+++ pronto/user.php
@@ -1,6 +1,6 @@
 <form action="" method="POST">
   <label for="email">Email:</label>
-  <input type="email" id="email" name="email" required />
+  <input type="email" id="mail" name="mail" required />
 
   <label for="username">Username:</label>
   <input type="text" id="username" name="username" required />
@@ -20,19 +20,25 @@
 <?php
 $pdo = require_once "assets/php/connect.php";
 
+session_start();
+
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $mail = $_POST['mail'];
     $username = $_POST['username'];
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $password = $_POST['password'];
 
-    $sql = "INSERT INTO users (username, name, surname, password, create_time) VALUES (:username, :name, :surname, :password, NOW())";
+    $sql = "INSERT INTO users (mail, username, name, surname, password, create_time) VALUES (:mail, :username, :name, :surname, :password, NOW())";
     $stmt = $pdo -> prepare($sql);
     $stmt->execute([
+        ':mail'=>$mail,
         ':username' => $username,
         ':name' => $name,
         ':surname' => $surname,
         ':password' => $password
     ]);
+
+    $_SESSION["username"] = $username;
 }
 ?>
